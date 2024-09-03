78e8a57cf5157d98f6e56b0b1b46520be769c9ff
Ekow korsah <51144050+ekow-korsah@users.noreply.github.com>
2024-09-03 16:19:52 -0300

    adding questions file

---
+++ Questions.py
@@ -0,0 +1,34 @@
+
+
+
+# Example 1: Print the first 10 natural numbers using for loop.
+
+
+# Example 2: Python program to print all the even numbers within the given range.
+
+
+
+# Example 3: Python program to calculate the sum of all numbers from 1 to a given number.
+
+
+
+# Example 4: Python program to calculate the sum of all the odd numbers within the given range.
+
+
+# Example 5: Python program to print a multiplication table of a given number
+
+
+# Example 6: Python program to display numbers from a list using a for loop.
+
+
+
+# Example 7: Python program to count the total number of digits in a number.
+
+
+
+
+# Example 8: Python program to check if the given string is a palindrome.
+
+
+
+# Example 8: Python program to check if the given string is a palindrome.
